b6dc6b8ddc3312f69dfd08c2a76ef8e7c9342c62
신동민/NE <dongmyo@nhnent.com>
2019-02-02 18:59:27 +0900

    * Entity 맵핑 - Item

---
+++ src/main/java/com/nhnent/edu/spring_jpa/entity/Item.java
@@ -0,0 +1,59 @@
+package com.nhnent.edu.spring_jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ *
+ * create table if not exists `Items` (
+ *   `item_id` bigint not null auto_increment,
+ *   `item_name` varchar(40) not null,
+ *   `price` bigint not null,
+ *
+ *   primary key(`item_id`)
+ * );
+ *
+ */
+@Entity
+@Table(name = "Items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long itemId;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    private Long price;
+
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+}
---
+++ src/test/java/com/nhnent/edu/spring_jpa/entity/ItemEntityTest.java
@@ -0,0 +1,28 @@
+package com.nhnent.edu.spring_jpa.entity;
+
+import com.nhnent.edu.spring_jpa.config.DatabaseConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { DatabaseConfig.class })
+@Transactional
+public class ItemEntityTest {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Test
+    public void test() throws Exception {
+        Item item1 = entityManager.find(Item.class, 1L);
+        Assert.assertNotNull(item1);
+        Assert.assertEquals(1L, item1.getItemId().longValue());
+    }
+}

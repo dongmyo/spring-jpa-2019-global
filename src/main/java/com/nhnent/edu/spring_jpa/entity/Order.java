c67d11dce0333db22a206d61c91c0e4afd72c3bc
신동민/NE <dongmyo@nhnent.com>
2019-02-02 19:26:06 +0900

    * Entity 맵핑 - Order

--- src/main/java/com/nhnent/edu/spring_jpa/entity/Order.java
+++ src/main/java/com/nhnent/edu/spring_jpa/entity/Order.java
@@ -1,5 +1,15 @@
 package com.nhnent.edu.spring_jpa.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
 // TODO : #1 `Orders` 테이블과 맵핑될 `Order` Entity 클래스를 작성하세요.
 /*
  * create table if not exists `Orders` (
@@ -10,5 +20,32 @@ package com.nhnent.edu.spring_jpa.entity;
  * );
  *
  */
+@Entity
+@Table(name = "Orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "order_date")
+    private Date orderDate;
+
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
 }
